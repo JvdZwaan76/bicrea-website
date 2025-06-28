1e98c70ddffd41bc9df826a7d09f610591fbfbf0
Jasper van der Zwaan <jaspervdz@me.com>
2025-06-27 18:03:05 -0700

    Revert to minimal Worker to restore functionality on 2025-06-27 17:55 PDT

--- worker.js
+++ worker.js
@@ -1,16 +1,5 @@
-import { Router } from 'itty-router';
-
-const router = Router();
-
-router.all('/favicon.ico', () => new Response(null, { status: 204 }));
-router.all('*', async (request) => {
-  const asset = await env.ASSETS.fetch(request);
-  return asset || new Response('Not found', { status: 404 });
-});
-
 export default {
   fetch(request) {
-    const response = router.handle(request);
-    return response;
+    return new Response("Hello from bicrea-website! Minimal test at 2025-06-27 17:22 PDT", { status: 200 });
   }
 };
