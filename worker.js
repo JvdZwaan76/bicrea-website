d78c6a40af15813b98aa7c8ab8fb168440a0cbbc
Jasper van der Zwaan <jaspervdz@me.com>
2025-06-27 18:09:56 -0700

    Reintroduce asset delegation with logging on 2025-06-27 18:08 PDT

--- worker.js
+++ worker.js
@@ -2,9 +2,11 @@ import { Router } from 'itty-router';
 
 const router = Router();
 
-router.get('/', () => new Response("Welcome to Bicrea homepage!", { status: 200 }));
 router.all('/favicon.ico', () => new Response(null, { status: 204 }));
-router.all('*', () => new Response('Not found', { status: 404 }));
+router.all('*', async (request) => {
+  const asset = await env.ASSETS.fetch(request);
+  return asset || new Response('Not found', { status: 404 });
+});
 
 export default {
   fetch(request) {
