dd717441ccb155d7804acb5c1b1d3dbcf017af3e
Jasper van der Zwaan <jaspervdz@me.com>
2025-06-27 17:51:52 -0700

    Reintroduce basic routing on 2025-06-27 17:46 PDT

--- worker.js
+++ worker.js
@@ -1,5 +1,14 @@
+import { Router } from 'itty-router';
+
+const router = Router();
+
+router.get('/', () => new Response("Welcome to Bicrea homepage!", { status: 200 }));
+router.all('/favicon.ico', () => new Response(null, { status: 204 }));
+router.all('*', () => new Response('Not found', { status: 404 }));
+
 export default {
   fetch(request) {
-    return new Response("Hello from bicrea-website! Minimal test at 2025-06-27 17:22 PDT", { status: 200 });
+    const response = router.handle(request);
+    return response;
   }
 };
